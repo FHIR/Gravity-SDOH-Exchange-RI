869e40e6be2bd4144fb43dd39c7cff0ec5cdc777
ruslan_didyk <ruslan.didyk@hevelian.com>
2021-04-04 15:32:20 +0300

    Revert back missed changes.

--- ehr/app/src/main/java/org/hl7/gravity/refimpl/sdohexchange/controller/LaunchController.java
+++ ehr/app/src/main/java/org/hl7/gravity/refimpl/sdohexchange/controller/LaunchController.java
@@ -1,9 +1,6 @@
 package org.hl7.gravity.refimpl.sdohexchange.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import springfox.documentation.annotations.ApiIgnore;
@@ -15,9 +12,6 @@ public class LaunchController {
 
   @GetMapping("/")
   public String index() {
-    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-    OAuth2User principal = (OAuth2User) authentication.getPrincipal();
-    System.out.println(principal.getAttributes());
     return "index";
   }
 }
