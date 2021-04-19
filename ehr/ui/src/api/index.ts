42364b39d14262fb9b5f527f17e1ee43a112c0d8
andriy-moskalov <andriy.moskalov@hevelian.com>
2021-04-19 14:46:06 +0300

    add task endpoint

--- ehr/ui/src/api/index.ts
+++ ehr/ui/src/api/index.ts
@@ -1,8 +1,14 @@
 import axios from "axios";
-import { ContextResponse } from "@/types";
+import { ContextResponse, TaskResponse } from "@/types";
 
 export const getContext = async (): Promise<ContextResponse> => {
 	const res = await axios.get("/api/current-context");
+
+	return res.data;
+};
+
+export const getTasks = async (): Promise<TaskResponse[]> => {
+	const res = await axios.get("/api/task");
 
 	return res.data;
 };
