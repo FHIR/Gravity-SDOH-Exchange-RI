434b6c4813c40b1cf2427764db187e9244da09bc
Mykhailo Stefantsiv <mykhailo.stefantsiv@hevelian.com>
2021-05-17 11:30:22 +0300

    CON-119: CORS Global Config added with TODO to improve

--- ehr/app/src/main/java/org/hl7/gravity/refimpl/sdohexchange/config/WebConfig.java
+++ ehr/app/src/main/java/org/hl7/gravity/refimpl/sdohexchange/config/WebConfig.java
@@ -4,7 +4,6 @@ import com.healthlx.smartonfhir.config.EnableSmartOnFhir;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 // Enable Smart-on-FHIR authorization.
@@ -16,6 +15,14 @@ public class WebConfig implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**");
+    //TODO: restrict CORS only for needed endpoint and needed HTTP methods
+    registry.addMapping("/**")
+        .allowedOrigins("*")
+        .allowedMethods("GET", "POST", "OPTIONS", "PUT")
+        .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
+            "Access-Control-Request-Headers")
+        .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+        .allowCredentials(true)
+        .maxAge(3600);
   }
 }
