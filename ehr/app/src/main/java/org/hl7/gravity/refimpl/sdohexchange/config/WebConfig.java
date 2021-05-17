9862e5c174c7e9c4aa0d5095cb084e904809749e
Mykhailo Stefantsiv <mykhailo.stefantsiv@hevelian.com>
2021-05-17 14:03:57 +0300

    CON-119 Moved SmartOnFhir config directly to an app

--- ehr/app/src/main/java/org/hl7/gravity/refimpl/sdohexchange/config/WebConfig.java
+++ ehr/app/src/main/java/org/hl7/gravity/refimpl/sdohexchange/config/WebConfig.java
@@ -1,13 +1,10 @@
 package org.hl7.gravity.refimpl.sdohexchange.config;
 
-import com.healthlx.smartonfhir.config.EnableSmartOnFhir;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// Enable Smart-on-FHIR authorization.
-@EnableSmartOnFhir
 // Enable @Scheduled annotation support needed for a periodic Task polling from CBROs.
 @EnableScheduling
 @Configuration
@@ -15,10 +12,9 @@ public class WebConfig implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    //TODO: restrict CORS only for needed endpoint and needed HTTP methods
-    registry.addMapping("/**")
+    registry.addMapping("/administration/convert")
         .allowedOrigins("*")
-        .allowedMethods("GET", "POST", "OPTIONS", "PUT")
+        .allowedMethods("POST", "OPTIONS")
         .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
             "Access-Control-Request-Headers")
         .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
