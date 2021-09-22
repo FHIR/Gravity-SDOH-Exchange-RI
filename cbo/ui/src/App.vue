3aa58cab1f2eacc1f9c057130d1f03a15758e234
Maksym Domaretskyi <maksym.domaretskyi@hevelian.com>
2021-09-22 16:59:29 +0300

    CBO: basic skeleton

---
+++ cbo/ui/src/App.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import "@/assets/scss/styles.scss";
+import { defineComponent } from "vue";
+export default defineComponent({
+	name: "App"
+});
+</script>
+
+<template>
+	<el-container
+		direction="vertical"
+		class="main-container"
+	>
+		<el-main>
+			<router-view />
+		</el-main>
+	</el-container>
+</template>
---
+++ cbo/ui/vue.config.js
@@ -0,0 +1,23 @@
+const CompressionPlugin = require("compression-webpack-plugin");
+const StylelintPlugin = require("stylelint-webpack-plugin");
+
+module.exports = {
+	lintOnSave: process.env.NODE_ENV === "development" ? "warning" : "default",
+	devServer: {
+		port: 8082,
+		proxy: {
+			"/*": {
+				target: "http://localhost:8082"
+				// changeOrigin: true
+			}
+		}
+	},
+	configureWebpack: {
+		plugins: [
+			new CompressionPlugin(),
+			new StylelintPlugin({
+				files: ["./src/**/*.{vue,scss}"]
+			})
+		]
+	}
+};
