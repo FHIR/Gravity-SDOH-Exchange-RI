acf636a8791b76054a5b760b1be2a174bd7cc338
Vlad Melnyk <w.vladyslavmelnyk@gmail.com>
2021-11-09 09:59:47 +0200

    CP: Created internal exception and changed logic in extract() method in TaskInfoBundleExtractor

--- components/fhir-common/src/main/java/org/hl7/gravity/refimpl/sdohexchange/fhir/extract/TaskInfoBundleExtractor.java
+++ components/fhir-common/src/main/java/org/hl7/gravity/refimpl/sdohexchange/fhir/extract/TaskInfoBundleExtractor.java
@@ -1,6 +1,7 @@
 package org.hl7.gravity.refimpl.sdohexchange.fhir.extract;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.ServiceRequest;
 import org.hl7.fhir.r4.model.Task;
@@ -8,39 +9,41 @@ import org.hl7.gravity.refimpl.sdohexchange.fhir.extract.TaskInfoBundleExtractor
 import org.hl7.gravity.refimpl.sdohexchange.util.FhirUtil;
 
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class TaskInfoBundleExtractor extends BundleExtractor<List<TaskInfoHolder>> {
 
   @Override
   public List<TaskInfoHolder> extract(Bundle bundle) {
-    Map<String, ServiceRequest> serviceRequestMap = FhirUtil.getFromBundle(bundle, ServiceRequest.class)
-        .stream()
-        .collect(Collectors.toMap(serviceRequest -> serviceRequest.getIdElement()
-            .getIdPart(), Function.identity()));
-
     return FhirUtil.getFromBundle(bundle, Task.class)
         .stream()
         .map(task -> {
-          ServiceRequest serviceRequest = serviceRequestMap.get(task.getFocus()
-              .getReferenceElement()
-              .getIdPart());
+          if (!(task.getFocus()
+              .getResource() instanceof ServiceRequest)) {
+            String reason = String.format("Task resource with id '%s' does not contain focus of type ServiceRequest.",
+                task.getIdElement()
+                    .getIdPart());
+            throw new TaskInfoBundleExtractorException(reason);
+          }
+          ServiceRequest serviceRequest = (ServiceRequest) task.getFocus()
+              .getResource();
           return new TaskInfoHolder(task, serviceRequest);
         })
         .collect(Collectors.toList());
   }
 
   @Getter
+  @RequiredArgsConstructor
   public static class TaskInfoHolder {
 
     private final Task task;
     private final ServiceRequest serviceRequest;
+  }
 
-    public TaskInfoHolder(Task task, ServiceRequest serviceRequest) {
-      this.task = task;
-      this.serviceRequest = serviceRequest;
+  public static class TaskInfoBundleExtractorException extends RuntimeException {
+
+    public TaskInfoBundleExtractorException(String message) {
+      super(message);
     }
   }
 }
