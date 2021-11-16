f9496ff98c383e73329d95a83beca034b9e2c0cd
Vlad Melnyk <w.vladyslavmelnyk@gmail.com>
2021-11-16 11:56:01 +0200

    CP: Created update() method in OurTaskController class

--- cp/app/src/main/java/org/hl7/gravity/refimpl/sdohexchange/controller/OurTaskController.java
+++ cp/app/src/main/java/org/hl7/gravity/refimpl/sdohexchange/controller/OurTaskController.java
@@ -4,15 +4,20 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.hl7.gravity.refimpl.sdohexchange.config.SpringFoxConfig;
+import org.hl7.gravity.refimpl.sdohexchange.dto.request.UpdateOurTaskRequestDto;
 import org.hl7.gravity.refimpl.sdohexchange.dto.response.TaskDto;
 import org.hl7.gravity.refimpl.sdohexchange.service.OurTaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import java.util.List;
 
@@ -23,17 +28,26 @@ import java.util.List;
 @Api(tags = {SpringFoxConfig.OUR_TASK_API_TAG})
 public class OurTaskController {
 
-  private final OurTaskService taskService;
+  private final OurTaskService ourTaskService;
 
   @GetMapping
   @ApiOperation(value = "Retrieve all Our Tasks.")
   public List<TaskDto> readAll() {
-    return taskService.readAll();
+    return ourTaskService.readAll();
   }
 
   @GetMapping("/{id}")
-  @ApiOperation(value = "Retrieve Task resource by id.")
+  @ApiOperation(value = "Retrieve Our Task resource by id.")
   public TaskDto read(@PathVariable @NotBlank(message = "Task id can't be empty.") String id) {
-    return taskService.read(id);
+    return ourTaskService.read(id);
+  }
+
+  @PutMapping("/{id}")
+  @ApiOperation(value = "Update Our Task resource.")
+  public ResponseEntity<Void> update(@PathVariable @NotBlank(message = "Task id can't be empty.") String id,
+      @RequestBody @Valid UpdateOurTaskRequestDto updateOurTaskRequestDto) {
+    ourTaskService.update(id, updateOurTaskRequestDto);
+    return ResponseEntity.noContent()
+        .build();
   }
 }
