c7701c0f2c30607feb45351b8f986e7e8ed84da1
Vlad Melnyk <w.vladyslavmelnyk@gmail.com>
2021-11-16 11:59:49 +0200

    CBO: Added @Validated and @NotBlank annotations to ResourcesController

--- cbo/app/src/main/java/org/hl7/gravity/refimpl/sdohexchange/controller/ResourcesController.java
+++ cbo/app/src/main/java/org/hl7/gravity/refimpl/sdohexchange/controller/ResourcesController.java
@@ -5,11 +5,15 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.hl7.gravity.refimpl.sdohexchange.dto.response.TaskJsonResourcesDto;
 import org.hl7.gravity.refimpl.sdohexchange.service.ResourceService;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.NotBlank;
+
+@Validated
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("resources")
@@ -20,7 +24,8 @@ public class ResourcesController {
 
   @ApiOperation(value = "Get all Task resources as JSON.")
   @GetMapping("/{serverId}/task/{taskId}")
-  public TaskJsonResourcesDto getTaskResources(@PathVariable Integer serverId, @PathVariable String taskId) {
+  public TaskJsonResourcesDto getTaskResources(@PathVariable Integer serverId,
+      @PathVariable @NotBlank(message = "Task id can't be empty.") String taskId) {
     return resourceService.getTaskResources(serverId, taskId);
   }
 }
