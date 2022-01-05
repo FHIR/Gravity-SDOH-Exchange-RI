d191d855b84ded94e0549cd94427bfe0760c5977
Vlad Melnyk <w.vladyslavmelnyk@gmail.com>
2022-01-05 13:29:39 +0200

    EHR: Added questionnaireResponse field to PatientTaskDto.java

--- ehr/app/src/main/java/org/hl7/gravity/refimpl/sdohexchange/dto/response/patienttask/PatientTaskDto.java
+++ ehr/app/src/main/java/org/hl7/gravity/refimpl/sdohexchange/dto/response/patienttask/PatientTaskDto.java
@@ -1,5 +1,6 @@
 package org.hl7.gravity.refimpl.sdohexchange.dto.response.patienttask;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -7,14 +8,17 @@ import org.hl7.gravity.refimpl.sdohexchange.dto.response.CommentDto;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Getter
 @Setter
 @RequiredArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class PatientTaskDto extends PatientTaskItemDto {
 
   private LocalDateTime createdAt;
   private List<CommentDto> comments = new ArrayList<>();
-
+  private Map<String, String> questionnaireResponse = new HashMap<>();
 }
