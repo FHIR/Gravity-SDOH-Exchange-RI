bb2a5a2eaae833cada0300dfae598770454b5eb2
Vlad Melnyk <w.vladyslavmelnyk@gmail.com>
2022-01-11 16:01:37 +0200

    EHR: OccurrenceResponseDto.java was formatted

--- ehr/app/src/main/java/org/hl7/gravity/refimpl/sdohexchange/dto/response/OccurrenceResponseDto.java
+++ ehr/app/src/main/java/org/hl7/gravity/refimpl/sdohexchange/dto/response/OccurrenceResponseDto.java
@@ -1,9 +1,10 @@
 package org.hl7.gravity.refimpl.sdohexchange.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @AllArgsConstructor
