a28fd9e6d36af0c6d1b2e30f11485384c84348bf
MelnykVL <w.vladyslavmelnyk@gmail.com>
2022-04-27 16:38:33 +0300

    Created UsCorePersonalCharacteristicProfile enum

---
+++ components/fhir-common/src/main/java/org/hl7/gravity/refimpl/sdohexchange/fhir/UsCorePersonalCharacteristicProfile.java
@@ -0,0 +1,19 @@
+package org.hl7.gravity.refimpl.sdohexchange.fhir;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class UsCorePersonalCharacteristicProfile {
+
+  public final String PERSONAL_PRONOUNS =
+      "http://hl7.org/fhir/us/sdoh-clinicalcare/StructureDefinition/SDOHCC-ObservationPersonalPronouns";
+  public final String ETHNICITY =
+      "http://hl7.org/fhir/us/sdoh-clinicalcare/StructureDefinition/SDOHCC-ObservationEthnicityOMB";
+  public final String RACE = "http://hl7.org/fhir/us/sdoh-clinicalcare/StructureDefinition/SDOHCC-ObservationRaceOMB";
+  public final String SEX_GENDER =
+      "http://hl7.org/fhir/us/sdoh-clinicalcare/StructureDefinition/SDOHCC-ObservationRecordedSexGender";
+  public final String SEXUAL_ORIENTATION =
+      "http://hl7.org/fhir/us/sdoh-clinicalcare/StructureDefinition/SDOHCC-ObservationSexualOrientation";
+  public final String GENDER_IDENTITY =
+      "http://hl7.org/fhir/us/sdoh-clinicalcare/StructureDefinition/SDOHCC-ObservationGenderIdentity";
+}
