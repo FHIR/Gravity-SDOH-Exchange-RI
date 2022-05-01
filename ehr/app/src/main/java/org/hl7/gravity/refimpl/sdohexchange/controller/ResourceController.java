54bfc8447f7185a434c258078d886d8816cf8d2e
MelnykVL <w.vladyslavmelnyk@gmail.com>
2022-05-01 23:04:24 +0300

    Created ResourceController.java

---
+++ ehr/app/src/main/java/org/hl7/gravity/refimpl/sdohexchange/controller/ResourceController.java
@@ -0,0 +1,31 @@
+package org.hl7.gravity.refimpl.sdohexchange.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.hl7.gravity.refimpl.sdohexchange.dto.response.characteristic.PersonalCharacteristicJsonResourcesDto;
+import org.hl7.gravity.refimpl.sdohexchange.service.ResourceService;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotBlank;
+
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("resources")
+@Api(tags = "Resources API")
+public class ResourceController {
+
+  private final ResourceService resourceService;
+
+  @ApiOperation(value = "Get all Personal Characteristic resources as JSON.")
+  @GetMapping("/{characteristicId}/characteristics")
+  public PersonalCharacteristicJsonResourcesDto getTaskResources(
+      @PathVariable @NotBlank(message = "Personal Characteristic id can't be empty.") String characteristicId) {
+    return resourceService.getCharacteristicResources(characteristicId);
+  }
+}
