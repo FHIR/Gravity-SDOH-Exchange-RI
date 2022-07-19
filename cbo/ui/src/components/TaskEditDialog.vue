47d83e897677fb5236a0e4546a7635660c0428ac
Yuriy Flyud <yuflyud@gmail.com>
2022-07-19 14:56:25 +0200

    Fixed task name display

--- cbo/ui/src/components/TaskEditDialog.vue
+++ cbo/ui/src/components/TaskEditDialog.vue
@@ -50,7 +50,7 @@ const showOccurrence = (ocr: Occurrence) => ocr.start ? `From ${showDate(ocr.sta
 
 const prepareTaskStuff = (task: Task): TaskStuff => ({
 	id: task.id,
-	requestName: task.name,
+	requestName: `${task.name || "N/A"}`,
 	requestor: task.requester.display,
 	request: `${task.serviceRequest.code.display || ""} (${task.serviceRequest.code.code})`,
 	forPatient: task.patient.display,
