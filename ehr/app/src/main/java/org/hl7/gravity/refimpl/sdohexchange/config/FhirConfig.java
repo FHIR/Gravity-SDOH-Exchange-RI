9ea24a6c5b0c6007745b4ec445184826ea891e79
Vanessa Fotso <vfotso@mitre.org>
2023-03-22 11:49:22 -0400

    temp disable auth

--- ehr/app/src/main/java/org/hl7/gravity/refimpl/sdohexchange/config/FhirConfig.java
+++ ehr/app/src/main/java/org/hl7/gravity/refimpl/sdohexchange/config/FhirConfig.java
@@ -26,27 +26,36 @@ public class FhirConfig {
   @Scope(scopeName = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
   public IGenericClient ehrClient(OAuth2AuthorizedClientService clientService, FhirContext fhirContext,
       @Value("${ehr.fhir-server-uri}") String fhirServerUri) {
-    Authentication authentication = SecurityContextHolder.getContext()
-        .getAuthentication();
-    String accessToken = null;
-    if (authentication.getClass()
-        .isAssignableFrom(OAuth2AuthenticationToken.class)) {
-      OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
-      String clientRegistrationId = oauthToken.getAuthorizedClientRegistrationId();
-      if (clientRegistrationId.equals("ehr-client")) {
-        OAuth2AuthorizedClient client = clientService.loadAuthorizedClient(clientRegistrationId, oauthToken.getName());
-        accessToken = client.getAccessToken()
-            .getTokenValue();
-      }
-    }
-    IGenericClient fhirClient = fhirContext.newRestfulGenericClient(fhirServerUri);
-    fhirClient.registerInterceptor(new BearerTokenAuthInterceptor(accessToken));
+    // TODO: THIS IS TEMPORARY SOLUTION. Add authentication once hapi server is
+    // updated
+    // Authentication authentication = SecurityContextHolder.getContext()
+    // .getAuthentication();
+    // String accessToken = null;
+    // if (authentication.getClass()
+    // .isAssignableFrom(OAuth2AuthenticationToken.class)) {
+    // OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken)
+    // authentication;
+    // String clientRegistrationId = oauthToken.getAuthorizedClientRegistrationId();
+    // if (clientRegistrationId.equals("ehr-client")) {
+    // OAuth2AuthorizedClient client =
+    // clientService.loadAuthorizedClient(clientRegistrationId,
+    // oauthToken.getName());
+    // accessToken = client.getAccessToken()
+    // .getTokenValue();
+    // }
+    // }
+    // IGenericClient fhirClient =
+    // fhirContext.newRestfulGenericClient(fhirServerUri);
+    // fhirClient.registerInterceptor(new BearerTokenAuthInterceptor(accessToken));
+    IGenericClient fhirClient = fhirContext.newRestfulGenericClient("http://localhost:8080/fhir");
     return fhirClient;
   }
 
   @Bean
   public IGenericClient openEhrClient(FhirContext fhirContext,
       @Value("${ehr.open-fhir-server-uri}") String fhirServerUri) {
-    return fhirContext.newRestfulGenericClient(fhirServerUri);
+    // TODO: THIS IS A TEMPORARY SOLUTION. Will be removed once hapi server is READY
+    // return fhirContext.newRestfulGenericClient(fhirServerUri);
+    return fhirContext.newRestfulGenericClient("http://localhost:8080/fhir");
   }
 }
